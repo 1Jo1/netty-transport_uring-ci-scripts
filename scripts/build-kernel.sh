bdd58506f59b03c5d0076c02a1213cd16824656f
Josef Grieb <josef.grieb@gmail.com>
2021-05-13 08:53:14 +0200

    add initramfs

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-wget https://gist.githubusercontent.com/1Jo1/049b60e3ca767e9ad71c676f81f47add/raw/a71dce14d2e16e1944fefd66fa108193a53c466c/linux_5_9-ubu.config
-mv linux_5_9-ubu.config .config
+wget https://gist.githubusercontent.com/1Jo1/57346c0d695adb5974a7f4e53f9ac2ac/raw/fb1fe4d8286192d429631edceae53f031c667c8a/linux-5_8-generic-config
+mv linux-5_8-generic-config .config
+
+version=$(make kernelversion)
 
 make ARCH=x86_64 olddefconfig
 make -j$(nproc)
+make modules_install
+update-initramfs -c -k "${version}"
+
 mkdir kernel-build && cp arch/x86/boot/bzImage kernel-build/$1-kernel-qemu-release
-
-
+mv /boot/initrd.img-"${version}" kernel-build/$1-initrd.img

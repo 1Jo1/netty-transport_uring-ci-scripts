f505c3cc807e987cce8ab2924e7c3b9d59dad70f
Josef Grieb <josef.grieb@gmail.com>
2021-06-19 08:41:59 +0200

    download user-data and meta-data

--- scripts/build-kernel-qemu.sh
+++ scripts/build-kernel-qemu.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 wget https://cloud-images.ubuntu.com/releases/hirsute/release/ubuntu-21.04-server-cloudimg-amd64.img
+
+wget https://raw.githubuserco.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/ubuntu-cloud/user-data
+wget https://raw.githubuserco.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/ubuntu-cloud/meta-data
+
 qemu-img create -f qcow2 -b ubuntu-21.04-server-cloudimg-amd64.img ubuntu.qcow2 70G
-genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock ubuntu-cloud/user-data ubuntu-cloud/meta-data
+genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-data
 
 mkdir ubuntu-kernel-image
 wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/build-kernel.exp && chmod +x build-kernel.exp

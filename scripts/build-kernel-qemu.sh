855ebf9fd738f4ea674a655994e3ea45656cefff
Josef Grieb <josef.grieb@gmail.com>
2021-06-19 09:11:50 +0200

    debugging

--- scripts/build-kernel-qemu.sh
+++ scripts/build-kernel-qemu.sh
@@ -8,6 +8,8 @@ wget https://raw.githubuserco.com/1Jo1/netty-transport_uring-ci-scripts/netty-ke
 qemu-img create -f qcow2 -b ubuntu-21.04-server-cloudimg-amd64.img ubuntu.qcow2 70G
 genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-data
 
+ls -la
+pwd
 mkdir ubuntu-kernel-image
 wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/build-kernel.exp && chmod +x build-kernel.exp
 expect -f build-kernel.exp `pwd` ubuntu.qcow2 ubuntu-seed.iso

10dd1a8f54bac8d134f8132dd611788cd6fc5b95
Josef Grieb <josef.grieb@gmail.com>
2021-06-21 16:41:32 +0200

    revert build-kernel-qemu

--- scripts/build-kernel-qemu.sh
+++ scripts/build-kernel-qemu.sh
@@ -11,14 +11,14 @@ wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/net
 qemu-img create -f qcow2 -b ubuntu-21.04-server-cloudimg-amd64.img ubuntu.qcow2 70G
 genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-data
 
-#wget https://gist.githubusercontent.com/1Jo1/cb1d0dc3ef5b824f5f7db3736d89c688/raw/746794634a628facd34c327fa6a4bf90d9a76ce6/ubuntu-config
-#mv ubuntu-config .config
+wget https://gist.githubusercontent.com/1Jo1/cb1d0dc3ef5b824f5f7db3736d89c688/raw/746794634a628facd34c327fa6a4bf90d9a76ce6/ubuntu-config
+mv ubuntu-config .config
 
 mkdir kernel-image
-#make olddefconfig
-#make -j 24
+make olddefconfig
+make -j 24
 
-#cp arch/x86/boot/bzImage kernel-image/
+cp arch/x86/boot/bzImage kernel-image/
 wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/build-kernel.exp && chmod +x build-kernel.exp
 echo $version
 expect -f build-kernel.exp `pwd` ubuntu.qcow2 ubuntu-seed.iso 24 $version

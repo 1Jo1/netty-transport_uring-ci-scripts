a7d60ffc10420c195a8e0718061f42092f06c186
Josef Grieb <josef.grieb@gmail.com>
2021-06-21 10:23:30 +0200

    use full path

--- scripts/build-kernel-qemu.sh
+++ scripts/build-kernel-qemu.sh
@@ -28,6 +28,6 @@ rm -rf ubuntu.qcow2 ubuntu-seed.iso
 qemu-img create -f qcow2 -b ubuntu-21.04-server-cloudimg-amd64.img ubuntu.qcow2 70G
 genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-data
 
-expect -f qemu.exp `pwd` ubuntu.qcow2 ubuntu-seed.iso kernel-build/initrd.img-"${version}" arch/x86/boot/bzImage
+expect -f qemu.exp `pwd` ubuntu.qcow2 ubuntu-seed.iso kernel-image/initrd.img-"${version}" kernel-image/bzImage
 
 

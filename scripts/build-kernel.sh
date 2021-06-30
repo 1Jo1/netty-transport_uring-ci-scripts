36d434e10561852d6e0c7050f1c74cd66774403f
Josef Grieb <josef.grieb@gmail.com>
2021-06-30 23:08:07 +0200

    remove ubuntu iso

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-wget https://cloud-images.ubuntu.com/releases/hirsute/release/ubuntu-21.04-server-cloudimg-amd64.img
 
 wget https://gist.githubusercontent.com/1Jo1/cb1d0dc3ef5b824f5f7db3736d89c688/raw/d88759776bc99787a35784d2cf64f2b7de306425/ubuntu-config
 mv ubuntu-config .config

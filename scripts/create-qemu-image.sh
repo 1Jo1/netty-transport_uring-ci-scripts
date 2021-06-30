aaf2c41c226c1e42fc783afb529fa3ce49e73fec
Josef Grieb <josef.grieb@gmail.com>
2021-06-30 23:07:24 +0200

    add ubuntu iso

--- scripts/create-qemu-image.sh
+++ scripts/create-qemu-image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+wget https://cloud-images.ubuntu.com/releases/hirsute/release/ubuntu-21.04-server-cloudimg-amd64.img
+
 qemu-img create -f qcow2 -b ubuntu-21.04-server-cloudimg-amd64.img ubuntu.qcow2 70G
 genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-data

93e2e2195ac14db1c47e8861f7c5fd65472eb6c5
Josef Grieb <josef.grieb@gmail.com>
2021-06-30 23:50:15 +0200

    add user-data meta-data

--- scripts/create-qemu-image.sh
+++ scripts/create-qemu-image.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 wget https://cloud-images.ubuntu.com/releases/hirsute/release/ubuntu-21.04-server-cloudimg-amd64.img
+wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/user-data
+wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/meta-data
+
 
 qemu-img create -f qcow2 -b ubuntu-21.04-server-cloudimg-amd64.img ubuntu.qcow2 70G
 genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-data
--- scripts/test-netty-qemu.sh
+++ scripts/test-netty-qemu.sh
@@ -1,6 +1,4 @@
 rm -rf user-data meta-data ubuntu.qcow2 ubuntu-seed.iso
-wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/user-data
-wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/meta-data
 qemu-img create -f qcow2 -b ubuntu-21.04-server-cloudimg-amd64.img ubuntu.qcow2 70G
 genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-data
 

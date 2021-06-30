302d6b0ea4e7f09cfe15deec75d900d684a0c17f
Josef Grieb <josef.grieb@gmail.com>
2021-07-01 01:17:17 +0200

    ls directory

--- scripts/test-netty-qemu.sh
+++ scripts/test-netty-qemu.sh
@@ -4,4 +4,6 @@ genisoimage -output ubuntu-seed.iso -volid cidata -joliet -rock user-data meta-d
 
 git clone https://github.com/1Jo1/netty-incubator-transport-io_uring.git
 
+ls -lha
+
 expect -f qemu.exp `pwd` ubuntu.qcow2 ubuntu-seed.iso kernel-image/initrd.img-"${version}" kernel-image/bzImage

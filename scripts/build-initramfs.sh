1c2e0749bf9f50daf0bccff127ceb6efe1a18ac4
Josef Grieb <josef.grieb@gmail.com>
2021-07-01 02:14:53 +0200

    add initramfs

--- scripts/build-initramfs.sh
+++ scripts/build-initramfs.sh
@@ -6,7 +6,8 @@ wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/net
 wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/ubuntu-cloud/meta-data
 
 version=$(make kernelversion)
-
+ls -lah
+pwd
 wget https://raw.githubusercontent.com/1Jo1/netty-transport_uring-ci-scripts/netty-kernel-testing/scripts/build-kernel.exp && chmod +x build-kernel.exp
 expect -f build-kernel.exp `pwd` ubuntu.qcow2 ubuntu-seed.iso 24 $version
 ls -lah kernel-image
